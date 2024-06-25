26ed14411836dc13bfae2ad33dbc87b7e4733bb2
Murphy D Klein <mdk238@cornell.edu>
2024-06-25 10:08:28 -0400

    Changed reward function slightly.

--- envs/evade_train_env.py
+++ envs/evade_train_env.py
@@ -51,7 +51,8 @@ class evadeTrainEnv(satGymEnv):
         self._obs = None
         self._rew = None
         self.initial_goal_distance = 0
-        self.min_distance = 0
+        self.min_distance = float('inf')
+        self.max_reward = 0
 
     def reset(self, **kwargs):
         if self.randomize_initial_state:
@@ -101,8 +102,12 @@ class evadeTrainEnv(satGymEnv):
         if dist < self.min_distance:
             self.min_distance = dist
             rew = 1/dist
+            self.max_reward = rew
         else:
-            rew = 0
+            #rew = 0
+            #Added something to perhaps help with the evader overshooting and drifting off after getting close to the goal.
+            reward = 1/dist
+            rew = reward - self.max_reward
 
         #rew = (1/dist) * (1 - self._step/1024) #/self.initial_goal_distance divide by timestep?
 
